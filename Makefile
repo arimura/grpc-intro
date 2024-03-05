d062ad8120c9da32adb9fd39e8825600608dc8db
arimura <33759663+arimura@users.noreply.github.com>
2024-03-05 17:34:55 +0900

    Add server template

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 gen:
 	protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative example.proto
+
+run-server:
+	go run server/server.go
--- server/server.go
+++ server/server.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/arimura/grpc_intro"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ToDoServer struct {
+	grpc_intro.UnimplementedToDoServer
+}
+
+func (s *ToDoServer) GetItem(context.Context, *grpc_intro.Cond) (*grpc_intro.Item, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetItem not implemented")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 50051))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	grpc_intro.RegisterToDoServer(grpcServer, &ToDoServer{})
+	grpcServer.Serve(lis)
 }
